acfdad049c151b8bc67705d8cb0d491893baa538
computinginsight <535517244@qq.com>
2021-05-01 13:30:13 +0800

    Update L2Train.m

--- L2Train.m
+++ L2Train.m
@@ -1,6 +1,5 @@
 function [D_,W,loss_list]=L2Train(trainFeature,trainLogicalLabel,op)
 D_=build_label_manifold(trainFeature,trainLogicalLabel,op.k); 
-%initial recovered Label Distribution
 W=rand(size(trainFeature,2),size(trainLogicalLabel,2));
 Y = zeros(size(trainFeature,2),size(trainLogicalLabel,2));
 E = W;
@@ -18,32 +17,23 @@ rho = 1.1;
         
 for i = 1:max_iter
     S = obtain_S(trainFeature,D_,op.k,op.alpha,op.alpha); 
-%     loss_list(i)= obj_func1(trainFeature,trainLogicalLabel,D_,S,W,op);
     D_ = update_D_old(trainFeature,trainLogicalLabel,D_,S,W,op.beta,op.alpha,op.lambda);
     W = update_W(trainFeature,D_,W,E,Y,op.beta,miu);
     E = update_E(W,Y,op.gamma,miu);
     Y = Y+miu*(W-E);
-    modProb = exp(trainFeature * W);  % size_sam * size_Y
+    modProb = exp(trainFeature * W); 
     sumProb = sum(modProb, 2);
-    modProb = modProb ./ (repmat(sumProb,[1 size(modProb,2)]));
-    
-    
-    %dual residual
-    convergence2(i,1)=norm(miu*(W-E),'fro');
-  
-    
-    %dual epsilon
+    modProb = modProb ./ (repmat(sumProb,[1 size(modProb,2)]));    
+    convergence2(i,1)=norm(miu*(W-E),'fro'); 
     epsilon_dual(i,1)=sqrt(d)*epsilon_abs+epsilon_rel*norm(Y,'fro');
-    
     if (convergence2(i,1)<=epsilon_dual(i,1))
         break;
     end
-    % update the Lagrange multiplier Y and regularization term miu
     miu = min(miu*rho, miu_max) ;
 
 end
 D_ = softmax(D_);
-sumD = sum(D_, 2); % sum of rows
+sumD = sum(D_, 2);
 D_ = scalecols(D_, 1 ./ sumD);
 end
 function modProb = scalecols(x, s)
