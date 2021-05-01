321f82616363dce4b4d85a095d24723b5cd406e3
computinginsight <535517244@qq.com>
2021-05-01 13:31:08 +0800

    Update UpdateWbfgsProcess.m

--- UpdateWbfgsProcess.m
+++ UpdateWbfgsProcess.m
@@ -1,6 +1,5 @@
 function [target,gradient] = UpdateWbfgsProcess(W)
-%Provide the target function and the gradient.
-% Load the data set.
+
 global X;
 global D;
 global ee;
@@ -8,8 +7,7 @@ global mm;
 global yy;
 global bb;
 
-modProb = exp(X * W);  % size_sam * size_Y
-    % modProb = X * W;
+modProb = exp(X * W);  
 sumProb = sum(modProb, 2);
 W(isnan(W)) = 1e-6;
 W(isinf(W)) = 1e10;
@@ -18,17 +16,9 @@ sumProb(isinf(sumProb)) = 1e10;
 modProb = modProb ./ (repmat(sumProb,[1 size(modProb,2)]));
 modProb(isnan(modProb)) = 1e-4;
 modProb(isinf(modProb)) = 1e10;
-
-%prediction
 target = bb*sum(sum((modProb-D).^2))+ mm/2*sum(sum((W-ee+yy/mm).^2));
 target = real(target);
 target(isnan(target)) = 0;
-% if isinf(target)
-%     target = 1e10;
-% end
-% if isnan(target)
-%     target = 1e-4;
-% end
 gradient = 2*bb*X'*((modProb - D).*(modProb-modProb.*modProb)) + mm*(W-ee+yy/mm) ;
 gradient = real(gradient);
 gradient(isnan(gradient)) = 0;
